71a302b42ff82a586ac7f4120c01e3f1ae6cfc8a
tsm40 <77738296+tsm40@users.noreply.github.com>
2024-12-03 23:48:58 -0500

    final st

--- pages/final.py
+++ pages/final.py
@@ -16,18 +16,15 @@ st.subheader('Data Exploration')
 st.write("We first examined the data we were working with to get a general sense of what features should be used for training and what preprocessing needed to be done. We noticed that the target variable SII value has a very uneven distribution, which we must keep in mind for the interpretability of our models. Other things of note were that our dataset contained many null values, which necessitates imputation. Finally, there was a significant portion of the data containing the exact values of the answers to the PCIAT questionnaire, which directly contributes to the mathematical calculation of the SII values. Thus, we decided to manually remove these columns so that our models will not be influenced by those direct values and instead will be trained more intelligently on more biometric data.")
 st.image("Figures/distribution.png", caption="SII value category distribution in training dataset.")
 st.subheader('Manual Feature Selection/Hand-Engineered Features')
-st.write("The dataset contained many repetitive columns, such as weight, height, and BMI. By manually removing redundant features (like height and weight, as they are encapsulated in the BMI column), we created a more streamlined and useful dataset. Additionally, we deleted rows and columns with more than 50% missing data to improve the dataset's quality.")
+st.write("The dataset contains many repetitive columns, such as weight, height, and BMI. By manually deleting redundant features (like height and weight due to having a BMI column), we created a more streamlined and useful dataset.")
 st.subheader('Filling Missing Data')
-st.write("For most columns, such as PCIAT-PCIAT and BIA BIA set data, we filled missing values with the average of the existing data. For some columns, like the CGAS-CGAS score, we used linear regression to predict the missing values based on other related features, including BIA-BIA_BMI, BIA-BIA_BMR, BIA-BIA_DEE, and BIA-BIA_TBW.")
-st.subheader("PCA for Dimensionality Reduction")
-st.write("We applied Principal Component Analysis (PCA) to reduce the dimensionality of our original dataset. By setting an appropriate threshold of 90% explained variance, we retained 31 principal components (PCs), which significantly reduced the number of features from 82 to a more manageable set.")
-st.subheader("Smote")
+st.write("Since our dataset had a lot of missing values, we needed to fill them in strategically. First, we removed columns with more than 50% missing values. Next, for most columns, such as PCIAT-PCIAT and BIA BIA set data, we filled missing values with the average of the existing data. For some columns, like the CGAS-CGAS score, we used linear regression to predict the missing values based on other related features, including BIA-BIA_BMI, BIA-BIA_BMR, BIA-BIA_DEE, and BIA-BIA_TBW.")
+st.subheader('SMOTE for Balancing Dataset')
 st.write("Because the original dataset was very skewed towards the 0 range of SII values, we used SMOTE to resample the dataset after value imputation. This allowed for much better predictions for less represented classes.")
-col1, col2 = st.columns(2)
-with col1:
-    st.image("Figures/CEV_PCA.png")
-with col2:
-    st.image("Figures/EV_PCA.png")
+st.subheader("PCA for Dimensionality Reduction")
+st.write("PCA was used to reduce the dimensionality of our original dataset. By setting an appropriate threshold of 90% accuracy, we retained 13 principal components (PCs) on the training dataset. We then fit the training and testing data to these loadings and generated new training and testing datasets. This significantly reduced the number of features to focus on, thus resulting in more manageable data.")
+
+st.image("Figures/PCANEW.png")
 
 st.header('ML Algorithms/Models Implemented')
 
@@ -35,10 +32,13 @@ st.subheader('Supervised Learning: Logistic Regression')
 st.write('In our data set, the goal is to categorize each person into ranges of SII values, which is a summary of the total scores each participant received indicating their believed problematic internet usage. In particular, there are 4 score ranges (0 for None, 1 for Mild, 2 for Moderate, and 3 for Severe). Because this is directly translated into a classification problem, we believed logistic regression would be a quick and simple starting point we could use. Moreover, for any other classification model, this simple logistic regression could be a strong benchmark to compare against.')
 st.subheader('Unsupervised Learning: Fuzzy K-means Clustering')
 st.write("Since the target variable SII value is categorical, the prediction task is well-suited to a k-means clustering method, where the clusters represent SII value score ranges. To categorize SII values into four score ranges (0-3), we applied fuzzy K-means clustering using k = 4. After preprocessing the data using missing value imputation and feature scaling, we chose the five most relevant features based on the preprocessing (BIA-BIA_ICW, BIA-BIA_TBW, BIA-BIA_FFM, BIA-BIA_BMR, and BIA-BIA_LST). Then, we calculated the membership probabilities for each sample and assigned each data point to the cluster it had the highest probability of being in based on those features. ")
+st.subheader('Supervised Learning: Neural Network')
+st.write("Given that we were trying to categorize into 4 categories as well as the fact that the dataset was pretty complex, we decided a good method would be a neural network. For the actual implementation, our structure was as follows: 31 features (post pca) → Fully connected layer (64 units) → Fully connected layer (32 units) → Dropout layer (20%) → 4 unit output layer. ")
+st.markdown("*Note: In our proposal, we originally claimed to create an SVM, but we decided to switch to a neural network out of interest.*")
 
 st.header('Results and Discussion')
 
-logreg_tab, fuzzy_tab = st.tabs(["Logistic Regression", "Fuzzy K-means Clustering"])
+logreg_tab, fuzzy_tab, nn_tab = st.tabs(["Logistic Regression", "Fuzzy K-means Clustering", "Neural Network"])
 with logreg_tab:
     st.subheader("Model Results")
     st.image("Figures/logreg_conf.png", caption="Logistic regression confusion matrix.")
@@ -55,18 +55,32 @@ with fuzzy_tab:
     st.write("Precision: macro: 23.7%, weighted: 42.2%")
     st.write("Recall: macro: 21%, weighted: 39.7%")
     st.subheader("Model Analysis")
-    st.write("For Fuzzy K-means, it is quite noticeable that the accuracy, precision, and recall metrics are all quite low, especially compared to logistic regression. We dropped all samples that had NaN values for the PCIAT-PCIAT_Total column, as this column helps us obtain our model accuracy as it represents the true cluster value (0-3) for each sample. We chose to use the 'BIA-BIA_ICW', 'BIA-BIA_TBW', 'BIA-BIA_FFM',  'BIA-BIA_BMR', and 'BIA-BIA_LST' features as these 5 features contributed the most to the first iteration of PCA. We noticed that adding additional features to our Fuzzy K-means model seemed to significantly decrease the training accuracy, and most of the samples we did predict the clustering for had NaN values for at least one of the 5 features. Imputation for the missing data in these samples may significantly improve the training and testing accuracies. We also noticed that the dataset resulting from the 2736 individuals who did have the PCIAT-PCIAT_Total was quite unbalanced as more than 50% of the samples had a true cluster value of 0 and only 34 individuals had a true cluster value of 3.")
+    st.write("For Fuzzy K-means, it is quite noticeable that the accuracy, precision, and recall metrics are all quite low, especially compared to logistic regression. We dropped all samples that had NaN values for the “target” column, as this column helps us obtain our model accuracy as it represents the true cluster value (0-3) for each sample. We chose to use the first 4 PCA features chosen given a balanced dataset of 5168 samples evenly split across 4 clusters, as they contributed the most to the first iteration of PCA, giving us the highest accuracy. We noticed that adding additional features to our Fuzzy K-means model seemed to significantly decrease the training accuracy.")
+    st.markdown("*(change tabs above for other model)*")
+with nn_tab:
+    st.subheader("Model Results")
+    st.image("Figures/nn_conf.png", caption="Neural network confusion matrix.")
+    df2 = pd.DataFrame([['0', '0.75', '0.85', '0.80'],
+                        ['1', '0.38', '0.23', '0.29'],
+                        ['2', '0.26', '0.18', '0.22'],
+                        ['3', '0.19', '0.86', '0.31']],
+                      columns=["Class", "Precision", "Recall", "f1-score"])
+    st.table(df2)
+    st.subheader("Model Analysis")
+    st.write(
+        "To begin our neural network, we actually began with a few steps of data processing. There were two primary reasons for this which we found from previous methods. Firstly, the dataset itself had many missing values. Secondly, it was also very imbalanced. To attempt to fix this, we did the following: value imputation, then SMOTE, then PCA, then finally Scaling. After doing so, we ran it through the previously described model structure. At surface level, we actually achieved lower accuracy compared to logistic regression. However, unlike logistic regression, we had some success in identifying less represented classes, which was the main goal after the midterm.")
     st.markdown("*(change tabs above for other model)*")
 
 st.header("Next Steps")
-st.subheader("Data Imputation")
-st.write("The next step in imputation is to identify appropriate predictors to improve the precision of missing value predictions by linear regression. We can also utilize KNN (K-Nearest Neighbors) to fill in the missing values. Additionally, we can apply different imputation methods to specific features as needed.")
 st.subheader("Logistic Regression")
-st.write("The next step for improving logistic regression would be adding balance to the dataset. We believe we can take two steps to do this. Firstly, we could subtract data points that belong to the first range of SII values. Secondly, with value imputation, we could gain a few data points back, which were previously removed due to there being null values. When added back, we would be able to gain data points that belong to other ranges.")
+st.write("Seeing as how logistic regression definitely benefited from the more diverse dataset, we believe that a finer tuned method of sampling would be the next step in improving regression. Specifically, being more careful in choosing which features to be used in which process steps. For example, maybe leaving out gender (a binary value) from things like PCA and value imputation.")
 st.subheader("Fuzzy K-Means Clustering")
-st.write("Given our lower-than-expected performance for our fuzzy K-means clustering model, we could take several steps to improve them by utilizing our PCA and other preprocessing techniques more effectively and handling missing values more rigorously. In terms of preprocessing, we could select our features more carefully by better incorporating PCA outputs with our K-means inputs rather than just picking the top few features. When it comes to missing values, experimenting with more advanced implementations like iterative imputation or K-nearest neighbors could provide a better representation of the underlying data. Utilizing supervised learning may be very helpful for Fuzzy K-means clustering.")
-st.subheader("Support Vector Machine")
-st.write("We will implement a support vector machine-based model to predict the SII value. This model will be evaluated using cross-validation accuracy and will be compared to the models we have implemented thus far.")
+st.write("Given our lower-than-expected performance for our fuzzy K-means clustering model, we could take several steps to improve them through a couple of ways, notably improving our feature selection, further experimenting with different preprocessing methods like K-nearest neighbors, and also using supervised techniques instead of unsupervised techniques. This time, we did balance the dataset well during training, but still achieved only a 31.4% accuracy. We believe that utilizing supervised learning may be very helpful for Fuzzy K-means clustering in this specific problem.")
+st.subheader("Neural Network")
+st.write("One thing we might want to experiment with is tuning hyperparameters a bit more. During development, we tuned them a little, but we didn’t generate many meaningful improvements beyond the initial parameters we set. The next step might be to tune the preprocessing a bit more. For example, we applied general preprocessing to all features, however, our dataset includes a few binary features like the season, something that might benefit from remaining untouched and not scaled.")
+
+st.header("Conclusion")
+st.write("In conclusion, we identified our neural network as the best performing overall model, as it had a relatively high accuracy across different classes. We may explore deep learning methods in the future to have more robust predictive power. Finally, we aim to submit to the Kaggle competition, on which the official leaderboard accuracy is still quite low, so who knows what might happen in the future...")
 
 st.link_button("Gaant Chart", "https://docs.google.com/spreadsheets/d/1NzZaNOShu23aP_18EbaMfo7mWibpnGhX/edit?usp=sharing&ouid=111674034097172792134&rtpof=true&sd=true")
 df = pd.DataFrame([['Chen Ye', 'Value imputation exploration and implementation'],
